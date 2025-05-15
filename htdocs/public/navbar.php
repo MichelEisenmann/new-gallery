ee7831ba5a78b06ca6281f4a25e214daab073781
Michel <michel@famille-eisenmann.net>
2025-05-15 12:25:21 +0200

    Adjustments based on screen size

--- htdocs/public/navbar.php
+++ htdocs/public/navbar.php
@@ -2,13 +2,13 @@
   <a href="<?= Translator::url('/index.php') ?>" class="gem-name w3-bar-item w3-button">GISÈLE EISENMANN MONTAGNÉ</a>
   
   <!-- the three bars that give access to the menu if we are on a mobile -->
-  <a class="w3-bar-item w3-button w3-hover-black w3-hide-medium w3-hide-large w3-right"
+  <a class="w3-bar-item w3-button w3-hover-black w3-hide-large w3-right"
      href="javascript:void(0);" onclick="toggleFunction()" title="Toggle Navigation Menu">
     <i class="fa fa-bars"></i>
   </a>
 
-  <!-- this is hidden if we are on a mobile -->
-  <div class="w3-right w3-hide-small">
+  <!-- the menu for large screens -->
+  <div class="w3-right w3-hide-medium w3-hide-small">
     <div class="w3-dropdown-hover">
       <button class="w3-button gem-menu"><?= Translator::t('Work') ?> <i class="fa fa-caret-down"></i></button>
       <div class="w3-dropdown-content w3-bar-block w3-card-4">
@@ -24,8 +24,8 @@
   </div>
 </div>
 
-<!-- this is the menu that will be displayed when not enough space is available (accessed through the 3 bars) -->
-<div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-right">
+<!-- the menu for small screens (accessed through the 3 bars) -->
+<div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large  w3-right">
   <a href="<?= Translator::url('/public/work.php') ?>"
      class="w3-bar-item w3-button" onclick="toggleFunction()"><?= Translator::t('Work') ?></a>
   <a href="<?= Translator::url('/public/news.php') ?>"
