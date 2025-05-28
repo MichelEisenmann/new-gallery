a2d730523ea84f0497c94c00bfb208b79a0f2c84
Michel <michel@famille-eisenmann.net>
2025-05-28 10:37:23 +0200

    Added css

--- deploy-miniterranora.sh
+++ deploy-miniterranora.sh
@@ -5,6 +5,7 @@ cp -r htdocs/*.php //10.0.0.43/web
 mkdir //10.0.0.43/web/private
 cp -r htdocs/private //10.0.0.43/web
 
+cp -r htdocs/public/*.css //10.0.0.43/web/public
 cp -r htdocs/public/*.php //10.0.0.43/web/public
 
 cp -r htdocs/public/images/*.csv //10.0.0.43/web/public/images
