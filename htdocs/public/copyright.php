14f85869b96d979b8402a7862752dc989037e2b4
Michel <michel@famille-eisenmann.net>
2025-05-29 11:44:27 +0200

    Simplified copyright

--- htdocs/public/copyright.php
+++ htdocs/public/copyright.php
@@ -3,15 +3,16 @@
   <?= Translator::t('copyrightview') ?>
   <a href="<?= Translator::url('/public/serie-momentsfeminins.php') ?>"><?= Translator::t('momentsfeminins') ?></a>
   <p>
-    <div class="w3-margin-bottom">
-      <a class="list-group-item" href="https://www.instagram.com/gem.eisenmann/">
-	<i class="fa fa-instagram fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i></a>
-      <a class="list-group-item" href="https://www.artmajeur.com/gisele-eisenmann-montagne">
+    <div class="">
+      <a class="gem-a" href="https://www.instagram.com/gem.eisenmann/">
+	<i class="fa fa-instagram fa-fw"></i>
+      </a>
+      <a class="gem-a" href="https://www.artmajeur.com/gisele-eisenmann-montagne">
         <img src="/public/images/web/artmajeur-com.png" class="ArtMajeur-fixed-size" alt="Art Majeur">		
       </a>
     </div>
   </p>
-  <p><i class="fa fa-copyright"></i>2025 by Gisele Eisenmann Montagné <br>Valbonne 06, France</br></p>
+    <i class="fa fa-copyright"></i>2025 by Gisele Eisenmann Montagné <br>Valbonne 06, France</br>
   
 </footer>
 
