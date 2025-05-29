57fd51e08d107722fbebec95b6cbf77a769e985a
Michel <michel@famille-eisenmann.net>
2025-05-29 12:13:15 +0200

    Rearranged about

--- htdocs/public/about.php
+++ htdocs/public/about.php
@@ -20,54 +20,46 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/5/w3.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
- <link rel="stylesheet" href="./global-style.css">
- <link rel="stylesheet" href="./serie-style.css">
+  
+  <link rel="stylesheet" href="./global-style.css">
+  <link rel="stylesheet" href="./serie-style.css">
+
   <style>
- 
-  .grid {
-      display: grid;
-      grid-template-columns: repeat(4, 1fr);
-      gap: 10px;
-      align-items: center;
-      justify-items: center;
-      margin: auto;
-  }
-  .grid img {
-      border: 1px solid #ccc;
-      box-shadow: 2px 2px 6px 0px  rgba(0,0,0,0.3);
-      max-width: 100%;
-  }
-  .grid img:nth-child(2) {
-      grid-column: span 4;
-  }
-
-
-    .limited {
-      height: 100px;
+    .grid {
+	display: grid;
+	grid-template-columns: repeat(4, 1fr);
+	gap: 10px;
+	align-items: center;
+	justify-items: center;
+	margin: auto;
     }
-
-    .gem-cliping { height: 100%; width: 200px; overflow: hidden; }
+    .grid img {
+	border: 1px solid #ccc;
+	box-shadow: 2px 2px 6px 0px  rgba(0,0,0,0.3);
+	max-width: 100%;
+    }
+    .grid img:nth-child(2) {
+	grid-column: span 4;
+    }
 
 
   </style>
   
   <body>
-
     <!-- Header -->
     <?php include("../public/navbar.php"); ?>
-	
     
     <!-- Page Content -->
-    <div class="w3-container">
+    <div class="w3-container w3-padding-32 w3-animate-opacity gem-animate gem-fixed-width">
 
       <!-- Theme grid -->
       <div class="w3-content w3-container " id="grid_theme">
         <main class="grid">
 	  <div>
-	     <a href="<?= Translator::url('/index.php') ?>">
-		 <img class="artiste to-be-signed" src="../public/images/web/gisele.jpg" alt="Artist's photo" /></a>
+	    <a href="<?= Translator::url('/index.php') ?>">
+	      <img class="artiste to-be-signed" src="../public/images/web/gisele.jpg" alt="Artist's photo" /></a>
    	  </div>
-      
+	  
 	  
 	  <div></div>
 	  <div>
@@ -77,18 +69,14 @@
 	  </div>
 	</main>
       </div>
-    </div>
 
-    
-    <!-- common part -->
-    <div class="w3-content w3-container" id="">
-      
-      <?= Translator::t('Introduction'); ?>
-
-     <?= Translator::t('Winner'); ?>	  
+      <!-- text -->
+      <div class="w3-content w3-container gem-justify">
+	<?= Translator::t('Introduction'); ?>
+	<?= Translator::t('Winner'); ?>	  
+      </div>
       
     </div>
-    
     
     <!-- Footer -->
     <?php include("../public/copyright.php"); ?>
