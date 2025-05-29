ba04a011a760003da43776716bd188b284411a14
Michel <michel@famille-eisenmann.net>
2025-05-29 11:39:11 +0200

    Reorganized the css files to avoid duplicates

--- htdocs/public/copyright.php
+++ htdocs/public/copyright.php
@@ -2,14 +2,16 @@
 <footer class="w3-center gem-footer">
   <?= Translator::t('copyrightview') ?>
   <a href="<?= Translator::url('/public/serie-momentsfeminins.php') ?>"><?= Translator::t('momentsfeminins') ?></a>
- <p>     <div class="w3-margin-bottom">
-        <a class="list-group-item" href="https://www.instagram.com/gem.eisenmann/">
-		<i class="fa fa-instagram fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i></a>
-	    <a class="list-group-item" href="https://www.artmajeur.com/gisele-eisenmann-montagne">
-          <img src="/public/images/web/artmajeur-com.png" class="ArtMajeur-fixed-size" alt="Art Majeur">		
-	      </a>
+  <p>
+    <div class="w3-margin-bottom">
+      <a class="list-group-item" href="https://www.instagram.com/gem.eisenmann/">
+	<i class="fa fa-instagram fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i></a>
+      <a class="list-group-item" href="https://www.artmajeur.com/gisele-eisenmann-montagne">
+        <img src="/public/images/web/artmajeur-com.png" class="ArtMajeur-fixed-size" alt="Art Majeur">		
+      </a>
+    </div>
   </p>
-   <p><i class="fa fa-copyright"></i>2025 by Gisele Eisenmann Montagné <br>Valbonne 06, France</br></p>
- 
+  <p><i class="fa fa-copyright"></i>2025 by Gisele Eisenmann Montagné <br>Valbonne 06, France</br></p>
+  
 </footer>
 
--- htdocs/public/serie-watermirror.php
+++ htdocs/public/serie-watermirror.php
@@ -65,6 +65,7 @@ $line_generator->serie_dico= $serie;  // will be used to browse exclusively amon
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/5/w3.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
+  <link rel="stylesheet" href="./global-style.css">    
   <link rel="stylesheet" href="./serie-style.css">    
       
   <style>
